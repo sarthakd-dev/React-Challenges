3c99f268afe6d28056265ffe70d8a5ec2645ba13
Sarthak Dixit <dev.sarthakd@gmail.com>
2024-05-16 00:35:24 +0530

    adding a minute hand

--- challenges/src/App.js
+++ challenges/src/App.js
@@ -4,35 +4,39 @@ import {useState, useEffect } from "react";
 var intervalId ;
 function App() {
 var [running, setrunning] = useState(false);
-  var [counter, setCounter] = useState(0);
- 
+  var [seconds, setSeconds] = useState(56);
+var [minutes, setMinutes] = useState(0);
   const togglefxn= ()=>{
     debugger
     setrunning(running => !running);
      if(!running){
        intervalId = setInterval(() => {
-       setCounter(counter =>counter +1);
-       console.log(counter);
+       
+       setSeconds(seconds =>seconds +1);
+      
+       console.log(seconds);
       
     }, 1000); 
   }else{
  // Clear the interval on unmount
  clearInterval(intervalId);
  setrunning(false);
- setCounter(0);
+ setMinutes(0);
+ setSeconds(0);
   //   return () => clearInterval(intervalId);
   // }, []); 
   }
    
 
   }
-  // useEffect(() => {
-  //   return () => {
-  //     if (running) {
-  //       clearInterval(intervalId);
-  //     }
-  //   };
-  // }, [running]);
+  useEffect(() => {
+    return () => {
+      if(seconds>59){
+        setSeconds(0);
+         setMinutes(minutes =>minutes +1);
+       }
+    };
+  }, [seconds]);
   return (
     <div className="App">
       <header className="App-header">
@@ -40,7 +44,7 @@ var [running, setrunning] = useState(false);
         <p>
           Use the timer button to start and stop the clock
         </p>
-        <div >{counter}</div>
+        <div>{minutes}min</div><div >{seconds}sec</div>
         <button style={{cursor: "pointer"}} onClick={() =>togglefxn()}>Start!</button>
       </header>
     </div>
