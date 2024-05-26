c4bba28a1fc025ee2e61337f3a536ed15c8c7b90
Sarthak Dixit <dev.sarthakd@gmail.com>
2024-05-26 16:00:48 +0530

    get/render data with axios

--- challenges/src/Components/Home.jsx
+++ challenges/src/Components/Home.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import Table from 'react-bootstrap/Table';
+import axios from 'axios'; 
+import Pagination from 'react-bootstrap/Pagination';
 
 export default function Home() {
+    const [data, setData] = useState([]);
+    const getData = async() =>{
+        const response = await axios.get("https://dummyjson.com/products");
+        console.log(response.data.products);
+        setData(response.data.products);
+    }
+    useEffect(()=>{
+     getData();
+    },[]);
   return (
    <>
    <div className="container">
@@ -10,19 +22,25 @@ export default function Home() {
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Id</th>
+          <th>Price</th>
+          <th>Title</th>
+          <th>Body</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
+       {
+       data.map((el, i)=> {
+       return (<>
+       <tr>
+        <td>{el.id}</td>
+        <td>{el.price}</td>
+       <td>{el.title}</td>
+       <td><img src={el.thumbnail} style={{width: 50, height: 50}} /></td>
+       </tr>
+       </>)
+       })
+       }
         
       </tbody>
     </Table>
