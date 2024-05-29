5a77664b244fb0cc674cf43764dcecd7977b8d11
Sarthak Dixit <dev.sarthakd@gmail.com>
2024-05-30 00:13:41 +0530

    finished with pagination understanding

--- challenges/src/Components/Home.jsx
+++ challenges/src/Components/Home.jsx
@@ -6,14 +6,41 @@ import Pagination from 'react-bootstrap/Pagination';
 
 export default function Home() {
     const [data, setData] = useState([]);
+    const [pageData, setpageData] = useState([]);
+    const [pageCount, setpageCount] = useState(0);
+    const [page, setpage] = useState(1);
+    
     const getData = async() =>{
         const response = await axios.get("https://dummyjson.com/products");
         console.log(response.data.products);
         setData(response.data.products);
+        setpageCount(Math.ceil(data.length/5));
+        setpageData(()=>{
+            return data.filter(x =>  x.id< page+5 && x.id>= page);
+        })
     }
+    const handlePrev = ( )=>{
+    if(page !=1 ){
+        setpage(page-1);
+        if(page>0){
+        setpageData(()=>{
+            return data.filter(x => x.id< page+5 && x.id>= page);
+        })
+    }}};
+    const handleNext = ( )=>{
+        if(page != pageCount ){
+             if(page>0){
+            setpageData(()=>{
+                return data.filter(x => x.id< page+5 && x.id>= page);
+            })
+        }; 
+        setpage(page+1);
+    }
+    }
+  
     useEffect(()=>{
      getData();
-    },[]);
+    },[pageData]);
   return (
    <>
    <div className="container">
@@ -30,7 +57,7 @@ export default function Home() {
       </thead>
       <tbody>
        {
-       data.map((el, i)=> {
+       pageData.map((el, i)=> {
        return (<>
        <tr>
         <td>{el.id}</td>
@@ -44,6 +71,15 @@ export default function Home() {
         
       </tbody>
     </Table>
+    </div>
+    <div className="d-flex justify-content-end">
+    <Pagination>
+      <Pagination.Prev onClick={handlePrev} />
+      
+      {/* <Pagination.Item>{13}</Pagination.Item> */}
+      
+      <Pagination.Next onClick={handleNext} />
+    </Pagination>
     </div>
     </div>
    
